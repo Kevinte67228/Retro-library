023c66be8c426beff2d394eea3d3cdeb7c2204d5
Kevin Te <mingming67228@gmail.com>
2026-06-28 21:51:28 +0800

    v40.37: update CACHE_NAME

--- GameVault/sw.js
+++ GameVault/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'gamevault-v40-36';
+const CACHE_NAME = 'gamevault-v40-37';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './GameVault_v40_36_index.html',
+  './GameVault_v40_37_index.html',
   './manifest.json',
   './manual.html'
 ];
