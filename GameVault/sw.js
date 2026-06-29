8a0352fd37d1392b6539f11adcdad1b2884a3c06
Kevin Te <mingming67228@gmail.com>
2026-06-29 22:28:15 +0800

    v40.47: Switch2 + unified validator + red warning - sw.js

--- GameVault/sw.js
+++ GameVault/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'gamevault-v40-46';
+const CACHE_NAME = 'gamevault-v40-47';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './GameVault_v40_46_index.html',
+  './GameVault_v40_47_index.html',
   './manifest.json',
   './manual.html'
 ];
