850b0f5487ef0e30e71c3476aea2f4aa7f2edcc6
Kevin Te <mingming67228@gmail.com>
2026-06-29 12:48:42 +0800

    v40.41d: debug toast - sw.js

--- GameVault/sw.js
+++ GameVault/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'gamevault-v40-40';
+const CACHE_NAME = 'gamevault-v40-41d';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './GameVault_v40_40_index.html',
+  './GameVault_v40_41d_index.html',
   './manifest.json',
   './manual.html'
 ];
