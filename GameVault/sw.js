5c9cac337703d87419dcc8a47c88a9cf20cfa290
Kevin Te <mingming67228@gmail.com>
2026-06-29 20:50:49 +0800

    v40.45: two-line code join - sw.js

--- GameVault/sw.js
+++ GameVault/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'gamevault-v40-44';
+const CACHE_NAME = 'gamevault-v40-45';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './GameVault_v40_44_index.html',
+  './GameVault_v40_45_index.html',
   './manifest.json',
   './manual.html'
 ];
