4ee0bec44f218e92acac29502673eed754a90e0c
Kevin Te <mingming67228@gmail.com>
2026-06-29 15:37:09 +0800

    v40.43: expanded game code formats - sw.js

--- GameVault/sw.js
+++ GameVault/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'gamevault-v40-42';
+const CACHE_NAME = 'gamevault-v40-43';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './GameVault_v40_42_index.html',
+  './GameVault_v40_43_index.html',
   './manifest.json',
   './manual.html'
 ];
