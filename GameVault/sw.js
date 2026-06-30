bbd66f742af7ffb135803efa80c282fabab45339
Kevin Te <mingming67228@gmail.com>
2026-06-30 23:26:53 +0800

    v42.01: form reorder + name cleaning + region-aware cover - sw.js

--- GameVault/sw.js
+++ GameVault/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'gamevault-v41-04';
+const CACHE_NAME = 'gamevault-v42-01';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './GameVault_v41_04_index.html',
+  './GameVault_v42_01_index.html',
   './manifest.json',
   './manual.html'
 ];
