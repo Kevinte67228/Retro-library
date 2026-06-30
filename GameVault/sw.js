df6177a98ca8e2c34e03f4330827642daa582611
Kevin Te <mingming67228@gmail.com>
2026-07-01 02:26:15 +0800

    v42.04: 更新 CACHE_NAME 與快取清單

--- GameVault/sw.js
+++ GameVault/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'gamevault-v42-03';
+const CACHE_NAME = 'gamevault-v42-04';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './GameVault_v42_03_index.html',
+  './GameVault_v42_04_index.html',
   './manifest.json',
   './manual.html'
 ];
