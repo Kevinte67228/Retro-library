bf9a6f55ccd5c188b3a89b20a9e09318715ba09f
Kevin Te <mingming67228@gmail.com>
2026-07-03 11:47:27 +0800

    deploy v42.19: CACHE_NAME bump

--- GameVault/sw.js
+++ GameVault/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'gamevault-v42-18';
+const CACHE_NAME = 'gamevault-v42-19';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './GameVault_v42_18_index.html',
+  './GameVault_v42_19_index.html',
   './manifest.json',
   './manual.html'
 ];
