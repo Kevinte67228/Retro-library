db701fb4b51c365215853c5a312bbd8109af9860
Kevin Te <mingming67228@gmail.com>
2026-07-05 01:25:10 +0800

    v48.01：CACHE_NAME 遞增

--- GameVault/sw.js
+++ GameVault/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'gamevault-v47-03';
+const CACHE_NAME = 'gamevault-v48-01';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './GameVault_v47_03_index.html',
+  './GameVault_v48_01_index.html',
   './manifest.json',
   './manual.html'
 ];
