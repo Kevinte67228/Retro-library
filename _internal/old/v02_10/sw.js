0d84c2be1f97667cd6f64a01b29fe9888fd46038
Kevin Te <mingming67228@gmail.com>
2026-07-25 03:36:41 +0800

    部署 v02_11（備份 v02_10 並清理輪替）

--- _internal/old/v02_10/sw.js
+++ _internal/old/v02_10/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'gamevault-v02-05';
+const CACHE_NAME = 'gamevault-v02-10';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './GameVault_v02_05_index.html',
+  './GameVault_v02_10_index.html',
   './manifest.json',
   './manual.html',
   './bg.webp'
