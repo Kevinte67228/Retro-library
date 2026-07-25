7dce92d682938dda839a13decb36898f1edfa435
Kevin Te <mingming67228@gmail.com>
2026-07-25 21:16:44 +0800

    部署 v02_17（備份 v02_16 並清理輪替）

--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'gamevault-v02-16';
+const CACHE_NAME = 'gamevault-v02-17';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './GameVault_v02_16_index.html',
+  './GameVault_v02_17_index.html',
   './manifest.json',
   './manual.html',
   './bg.webp'
