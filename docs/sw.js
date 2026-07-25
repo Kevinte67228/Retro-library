f308eee6465fd0104b7818a89d64bb3ed7d8766d
Kevin Te <mingming67228@gmail.com>
2026-07-26 07:50:17 +0800

    部署 v02_28（備份 v02_27 並清理輪替）

--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'gamevault-v02-27';
+const CACHE_NAME = 'gamevault-v02-28';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './GameVault_v02_27_index.html',
+  './GameVault_v02_28_index.html',
   './manifest.json',
   './manual.html',
   './bg.webp'
