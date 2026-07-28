461e34d2bd5006a7c2162dcb99abb06fbc4912a9
Kevin Te <mingming67228@gmail.com>
2026-07-28 23:10:00 +0800

    部署 v02_41（備份 v02_40 並清理輪替）

--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'retrovault-v02-40';
+const CACHE_NAME = 'retrovault-v02-41';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './RetroVault_v02_40_index.html',
+  './RetroVault_v02_41_index.html',
   './manifest.json',
   './manual.html',
   './bg.webp'
