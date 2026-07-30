588b4ec64f62a83a3758824f8fa7149b74a8e951
Kevin Te <mingming67228@gmail.com>
2026-07-31 00:54:32 +0800

    部署 v02_53（備份 v02_52 並清理輪替）

--- _internal/old/v02_52/sw.js
+++ _internal/old/v02_52/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'retrovault-v02-47';
+const CACHE_NAME = 'retrovault-v02-52';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './RetroVault_v02_47_index.html',
+  './RetroVault_v02_52_index.html',
   './manifest.json',
   './manual.html',
   './bg.webp'
--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'retrovault-v02-52';
+const CACHE_NAME = 'retrovault-v02-53';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './RetroVault_v02_52_index.html',
+  './RetroVault_v02_53_index.html',
   './manifest.json',
   './manual.html',
   './bg.webp'
