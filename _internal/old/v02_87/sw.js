1d0b2994c056bc38686fcd8817533961537d4bd3
Kevin Te <mingming67228@gmail.com>
2026-08-06 22:13:28 +0800

    部署 v02.88（備份 v02_87 並清理輪替）

--- _internal/old/v02_87/sw.js
+++ _internal/old/v02_87/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'retrovault-v02-82';
+const CACHE_NAME = 'retrovault-v02-87';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './RetroVault_v02_82_index.html',
+  './RetroVault_v02_87_index.html',
   './manifest.json',
   './manual.html',
   './bg.webp'
