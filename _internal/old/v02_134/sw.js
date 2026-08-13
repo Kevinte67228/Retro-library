77e995b7d11c6840b2d7e81f87572be445ae4dcc
Kevin Te <mingming67228@gmail.com>
2026-08-13 17:18:51 +0800

    部署 v02_135（備份 v02_134 並清理輪替）

--- _internal/old/v02_134/sw.js
+++ _internal/old/v02_134/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'retrovault-v02-129';
+const CACHE_NAME = 'retrovault-v02-134';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './RetroVault_v02_129_index.html',
+  './RetroVault_v02_134_index.html',
   './manifest.json',
   './manual.html',
   './bg.webp'
