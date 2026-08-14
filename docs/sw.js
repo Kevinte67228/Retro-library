66b3d230093bf34b644acb077b9047dd17e3f348
Kevin Te <mingming67228@gmail.com>
2026-08-14 16:44:22 +0800

    部署 v02_147（備份 v02_146 並清理輪替）

--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'retrovault-v02-146';
+const CACHE_NAME = 'retrovault-v02-147';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './RetroVault_v02_146_index.html',
+  './RetroVault_v02_147_index.html',
   './manifest.json',
   './manual.html',
   './bg.webp'
